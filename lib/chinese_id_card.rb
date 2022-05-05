99319ce437e9339d18e6996dacae91ee3c9852b1
ian <ianlynxk@gmail.com>
2022-05-05 17:48:15 +0800

    init...

---
+++ lib/chinese_id_card.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "chinese_id_card/version"
+
+module ChineseIdCard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/chinese_id_card/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ChineseIdCard
+  VERSION = "0.1.0"
+end
